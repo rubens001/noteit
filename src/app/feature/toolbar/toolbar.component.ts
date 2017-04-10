ea75ae2c51af663fc50f3cb8ce51b957c615535c
Rubens <rubens001@gmail.com>
2017-04-10 15:55:25 -0300

    toolbar

--- src/app/feature/toolbar/toolbar.component.ts
+++ src/app/feature/toolbar/toolbar.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  menuBars = 'hamburger hamburger--elastic'; // 'fa fa-bars fa-2x';
+  menuClass = this.menuBars;
+  isSidenav = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onMenu = function(sidenav : any) {
+    this.isSidenav = !this.isSidenav;
+    if (this.isSidenav) {
+      this.menuClass = 'hamburger hamburger--elastic is-active'; // "fa fa-times fa-2x";
+      sidenav.open();
+    } else {
+      this.menuClass = this.menuBars;
+      sidenav.close();
+    }
+  }
+  onCloseSidenav() {
+    this.isSidenav=false;
+    this.menuClass = this.menuBars;
+  }
 }
